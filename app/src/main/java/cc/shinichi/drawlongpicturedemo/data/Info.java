4d4ebcc2998afbddd99dfd2df628f04afde5f4f4
工藤 <18883840501@163.com>
2019-08-09 13:25:29 +0800

    升级gradle配置

--- app/src/main/java/cc/shinichi/drawlongpicturedemo/data/Info.java
+++ app/src/main/java/cc/shinichi/drawlongpicturedemo/data/Info.java
@@ -5,29 +5,29 @@ import java.util.List;
 
 /**
  * @author 工藤
- * @email gougou@16fan.com
+ * @email 18883840501@163.com
  * cc.shinichi.drawlongpicturedemo.data
  * create at 2018/8/27  18:07
  * description:
  */
 public class Info implements Serializable {
 
-	String content;
-	List<String> imageList;
+    String content;
+    List<String> imageList;
 
-	public String getContent() {
-		return content;
-	}
+    public String getContent() {
+        return content;
+    }
 
-	public void setContent(String content) {
-		this.content = content;
-	}
+    public void setContent(String content) {
+        this.content = content;
+    }
 
-	public List<String> getImageList() {
-		return imageList;
-	}
+    public List<String> getImageList() {
+        return imageList;
+    }
 
-	public void setImageList(List<String> imageList) {
-		this.imageList = imageList;
-	}
+    public void setImageList(List<String> imageList) {
+        this.imageList = imageList;
+    }
 }
--- app/src/main/java/cc/shinichi/drawlongpicturedemo/util/PhoneUtil.java
+++ app/src/main/java/cc/shinichi/drawlongpicturedemo/util/PhoneUtil.java
@@ -6,36 +6,36 @@ import android.view.WindowManager;
 
 /**
  * @author 工藤
- * @email gougou@16fan.com
+ * @email 18883840501@163.com
  * cc.shinichi.drawlongpicturedemo.util
  * create at 2018/8/27  17:53
  * description:
  */
 public class PhoneUtil {
 
-	public static int getPhoneWid(Context context) {
-		WindowManager windowManager =
-			(WindowManager) context.getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
-		DisplayMetrics metric = new DisplayMetrics();
-		windowManager.getDefaultDisplay().getMetrics(metric);
-		return metric.widthPixels;
-	}
+    public static int getPhoneWid(Context context) {
+        WindowManager windowManager =
+            (WindowManager) context.getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics metric = new DisplayMetrics();
+        windowManager.getDefaultDisplay().getMetrics(metric);
+        return metric.widthPixels;
+    }
 
-	public static int getPhoneHei(Context context) {
-		WindowManager windowManager =
-			(WindowManager) context.getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
-		DisplayMetrics metric = new DisplayMetrics();
-		windowManager.getDefaultDisplay().getMetrics(metric);
-		return metric.heightPixels;
-	}
+    public static int getPhoneHei(Context context) {
+        WindowManager windowManager =
+            (WindowManager) context.getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics metric = new DisplayMetrics();
+        windowManager.getDefaultDisplay().getMetrics(metric);
+        return metric.heightPixels;
+    }
 
-	public static int px2dp(Context context, float pxValue) {
-		final float scale = context.getApplicationContext().getResources().getDisplayMetrics().density;
-		return (int) (pxValue / scale + 0.5f);
-	}
+    public static int px2dp(Context context, float pxValue) {
+        final float scale = context.getApplicationContext().getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
 
-	public static int dp2px(Context context, float dipValue) {
-		final float scale = context.getApplicationContext().getResources().getDisplayMetrics().density;
-		return (int) (dipValue * scale + 0.5f);
-	}
+    public static int dp2px(Context context, float dipValue) {
+        final float scale = context.getApplicationContext().getResources().getDisplayMetrics().density;
+        return (int) (dipValue * scale + 0.5f);
+    }
 }
